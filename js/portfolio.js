35910a2cf53ac9a63c42d03cf8975e9a92f66741
Gerald Michelant <gerald.michelant@gmail.com>
2017-12-04 00:56:08 -0500

    ecommerce project added

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,3 +3,8 @@ $(".navbar a").on("click", function(){
     $(".nav-item.active").removeClass("active");
     $(this).parent().addClass("active");
  });
+
+ $(document).on("click", ".open-AddImgDialog", function () {
+    var imgsrc = $(this).data('id');
+    $('#my_image').attr('src',imgsrc);
+   });
